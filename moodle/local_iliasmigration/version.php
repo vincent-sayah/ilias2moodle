7ad5ddf5ae4802c2ea795f1ace872da80c94f691
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-04 17:03:52 +0200

    feat: start Moodle Phase 2 dry-run importer

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -4,7 +4,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_iliasmigration';
-$plugin->version = 2026090400;
-$plugin->requires = 2024100700; // Moodle 4.5.
+$plugin->version = 2026090401;
+$plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.1.0-alpha';
+$plugin->release = '0.2.0-alpha';
