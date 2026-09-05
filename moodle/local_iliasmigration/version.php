99aeef16958800d9322f4c8308b21ec3038bb310
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-05 14:46:35 +0200

    chore: bump plugin to 0.8.1-alpha

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -1,10 +1,7 @@
 <?php
-// This file is part of ILIAS2Moodle.
-
-defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_iliasmigration';
 $plugin->version = 2026090508;
 $plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.8.0-alpha';
+$plugin->release = '0.8.1-alpha';
