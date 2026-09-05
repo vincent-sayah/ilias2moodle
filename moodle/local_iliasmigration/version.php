ccfcfd55ec40c4fe80854fca021bee4b605cd028
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-05 21:47:04 +0200

    Release Phase 6 guarded apply

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin->component = 'local_iliasmigration';
-$plugin->version = 2026090516;
+$plugin->version = 2026090517;
 $plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.11.1-alpha';
+$plugin->release = '0.12.0-alpha';
