e4586caa3f3d41c5d897a750e93ce9e37e1214a9
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-05 18:02:52 +0200

    Release 0.10.1-alpha Book import prerequisite check

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin->component = 'local_iliasmigration';
-$plugin->version = 2026090511;
+$plugin->version = 2026090512;
 $plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.10.0-alpha';
+$plugin->release = '0.10.1-alpha';
