de4a6986ebb7743264c04d21103b189e4994d2f2
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-05 14:19:18 +0200

    chore: release Phase 4 SCORM apply

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -4,7 +4,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_iliasmigration';
-$plugin->version = 2026090507;
+$plugin->version = 2026090508;
 $plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.7.1-alpha';
+$plugin->release = '0.8.0-alpha';
