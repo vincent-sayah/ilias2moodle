f9e780eb2ca8c6ad78b7efb0cf215fe853612ec4
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-06 09:46:42 +0200

    Release Phase 6 Quiz sumgrades recomputation fix

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin->component = 'local_iliasmigration';
-$plugin->version = 2026090521;
+$plugin->version = 2026090522;
 $plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.12.4-alpha';
+$plugin->release = '0.12.5-alpha';
