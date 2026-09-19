a9ad152161e4d1bfebb4fd3c33032b70370dd4a5
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-19 09:34:46 +0200

    Bump plugin for incremental Wiki prerequisites

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin->component = 'local_iliasmigration';
-$plugin->version = 2026091900;
+$plugin->version = 2026091901;
 $plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.16.11-alpha';
+$plugin->release = '0.16.12-alpha';
