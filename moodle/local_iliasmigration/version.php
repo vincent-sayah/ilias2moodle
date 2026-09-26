63e1268dfec79dbb876c6450e35e06ccb093da18
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-26 12:02:27 +0200

    Promote plugin to 0.20.0 RC1

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -9,7 +9,7 @@
  */
 
 $plugin->component = 'local_iliasmigration';
-$plugin->version = 2026092603;
+$plugin->version = 2026092604;
 $plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.20.0-alpha';
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '0.20.0-rc1';
