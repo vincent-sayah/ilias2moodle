8677ccd04c89dc3a397c93114c4e3d9bbb4d6f49
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-26 19:55:07 +0200

    Sort shared export-set imports

--- src/ilias2moodle/ilias/exercise.py
+++ src/ilias2moodle/ilias/exercise.py
@@ -6,9 +6,9 @@ from pathlib import PurePosixPath
 from typing import Any
 from xml.etree import ElementTree as ET
 
-from ilias2moodle.ilias.export_sets import find_export_sets
 
 from ilias2moodle.ilias.content_page import _local_name, _text_descendant
+from ilias2moodle.ilias.export_sets import find_export_sets
 
 RESOURCE_COLLECTION_UUID_RE = re.compile(
     r"^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
