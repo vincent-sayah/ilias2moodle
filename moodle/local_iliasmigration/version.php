64618991c3ec2e76c8db46ab610e4c2e78922bdf
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-09-26 11:38:15 +0200

    Bump plugin version for Phase 7.6 Wiki

--- moodle/local_iliasmigration/version.php
+++ moodle/local_iliasmigration/version.php
@@ -9,7 +9,7 @@
  */
 
 $plugin->component = 'local_iliasmigration';
-$plugin->version = 2026092602;
+$plugin->version = 2026092603;
 $plugin->requires = 2024100700; // Moodle 4.5 minimum; POC validated on Moodle 5.0.2.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.19.0-alpha';
+$plugin->release = '0.20.0-alpha';
